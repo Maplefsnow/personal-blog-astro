af8c43734d7bcb634afff8534c3dcd06a5eca8cf
Maplef <maplefsnow@gmail.com>
2024-08-30 16:17:57 +0000

    qwq

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://www.maplef.top/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
