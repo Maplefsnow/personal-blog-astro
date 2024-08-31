c924fda76bd7c41cf73d68cf9f5ac8ba6308be3d
Maplef <maplefsnow@gmail.com>
2024-08-31 15:47:44 +0000

    [update] try to add comment but failed

---
+++ src/constants/giscus.ts
@@ -0,0 +1,4 @@
+export const GISCUS_REPO = "Maplefsnow/personal-blog-astro"
+export const GISCUS_REPO_ID = "R_kgDOMqhwtA"
+export const GISCUS_CATEGORY_ID = "DIC_kwDOMqhwtM4CiHLp"
+export const GISCUS_LANG = "zh-CN"
